46140e9985d460b8fc24be5a048ba2bbd0444671
Quentin <buiquentinmacduyrgmail.com>
2025-11-19 00:15:24 +0100

    2eme commit modif de CvEntry

--- app/components/CvEntry.tsx
+++ app/components/CvEntry.tsx
@@ -1,12 +1,20 @@
-interface CvEntry {
-   
+type CvEntryProps ={
+  title: string;
+  date: string;
+  description: string;
 }
 
-export default function CvEntry(props: CvEntry) {
+export default function CvEntry(props: CvEntryProps) {
   return (
     <div className="p-4 border border-slate-200 rounded-xl shadow-sm mx-4 my-8">
       <div className="flex items-center justify-between">
+        <h3 className="text-lg font-semibold">{props.title}</h3>
+        <span className="text-slate-500">{props.date}</span>
       </div>
+
+      <p className="mt-2 text-slate-600">
+        {props.description}
+      </p>
     </div>
   );
 }

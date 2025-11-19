5b472f9a677c5129193157fd10a761017024a358
Quentin <buiquentinmacduyrgmail.com>
2025-11-19 23:46:12 +0100

    2eme commit

--- app/components/CvEntry.tsx
+++ app/components/CvEntry.tsx
@@ -12,7 +12,7 @@ export default function CvEntry(props: CvEntryProps) {
         <span className="text-slate-500">{props.date}</span>
       </div>
 
-      <p className="mt-2 text-slate-600">
+      <p className="mt-2 text-slate-600 whitespace-pre-line">
         {props.description}
       </p>
     </div>
